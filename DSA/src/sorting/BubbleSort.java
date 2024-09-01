9dc407a688b2b764e2cf396135948b6679eda41f
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-09-01 10:07:54 +0500

    Bubble Sort improved

--- DSA/src/sorting/BubbleSort.java
+++ DSA/src/sorting/BubbleSort.java
@@ -5,12 +5,17 @@ public class BubbleSort {
         int[] numbers = {88, 77, 66, 55, 44, 33, 22, 11, 87, 555, 222, 1, 2, 0};
 
         for (int i = 0; i < numbers.length - 1; i++) {
+            boolean swapped = false;
             for (int j = 0; j < numbers.length - 1 - i; j++) {
                 if (numbers[j] > numbers[j + 1]) {
                     int temp = numbers[j];
                     numbers[j] = numbers[j + 1];
                     numbers[j + 1] = temp;
+                    swapped = true;
                 }
+            }
+            if (!swapped) {
+                break;
             }
         }
 
