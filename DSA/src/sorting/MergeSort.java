35f6b5441299e0b47942e4e8ca823df0cfbbb00b
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-09-03 09:20:51 +0500

    Merge Sort (Conquer)

--- DSA/src/sorting/MergeSort.java
+++ DSA/src/sorting/MergeSort.java
@@ -16,6 +16,37 @@ public class MergeSort {
     }
 
     public static void merge(int[] data, int start, int mid, int end) {
+        int[] temp = new int[end - start + 1];
 
+        int i = start;
+        int j = mid + 1;
+        int k = 0;
+
+        while (i <= mid && j <= end) {
+            if (data[i] <= data[j]) {
+                temp[k] = data[i];
+                i++;
+            } else {
+                temp[k] = data[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i <= mid) {
+            temp[k] = data[i];
+            i++;
+            k++;
+        }
+
+        while (j <= end) {
+            temp[k] = data[i];
+            j++;
+            k++;
+        }
+
+        for (i = start; i <= end; i++) {
+            data[i] = temp[i - start];
+        }
     }
 }
