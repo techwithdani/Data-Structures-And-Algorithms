e376a4571c5c5dd8c9a323650b9026fee4ea4218
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-09-08 20:45:19 +0500

    Practice

--- DSA/src/Main.java
+++ DSA/src/Main.java
@@ -2,55 +2,41 @@ public class Main {
     public static void main(String[] args) {
         int[] numbers = {9, 7, 5, 8, 2, 1, 0};
 
-        mergeSort(numbers, 0, numbers.length - 1);
+        quickSort(numbers, 0, numbers.length - 1);
 
-        for (int number : numbers) {
-            System.out.print(number + " ");
+        for (int i : numbers) {
+            System.out.print(i + " ");
         }
     }
 
-    public static void mergeSort(int[] data, int start, int end) {
+    public static void quickSort(int[] data, int start, int end) {
         if (start < end) {
-            int mid = start + (end - start) / 2;
-            mergeSort(data, start, mid);
-            mergeSort(data, mid + 1, end);
-            merge(data, start, mid, end);
+            int pivot = partition(data, start, end);
+
+            quickSort(data, start, pivot - 1);
+            quickSort(data, pivot + 1, end);
         }
     }
 
-    public static void merge(int[] data, int start, int mid, int end) {
-        int[] temp = new int[end - start + 1];
+    public static int partition(int[] data, int start, int end) {
+        int pivot = data[end];
+        int i = start - 1;
+        int temp;
 
-        int i = start;
-        int j = mid + 1;
-        int k = 0;
-
-        while (i <= mid && j <= end) {
-            if (data[i] <= data[j]) {
-                temp[k] = data[i];
+        for (int j = start; j < end; j++) {
+            if (data[j] < pivot) {
                 i++;
-            } else {
-                temp[k] = data[j];
-                j++;
+                temp = data[i];
+                data[i] = data[j];
+                data[j] = temp;
             }
-            k++;
         }
+        i++;
 
-        while (i <= mid) {
-            temp[k] = data[i];
-            i++;
-            k++;
-        }
+        temp = data[i];
+        data[i] = data[end];
+        data[end] = temp;
 
-        while (j <= end) {
-            temp[k] = data[j];
-            j++;
-            k++;
-        }
-
-        for (i = start; i <= end; i++) {
-            data[i] = temp[i - start];
-        }
+        return i;
     }
-
 }
