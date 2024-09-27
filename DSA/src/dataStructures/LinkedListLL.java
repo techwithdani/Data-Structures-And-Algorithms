ed61aa70487a362f41fc33d1f3854208bc1d9a10
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-09-27 14:38:40 +0500

    Linked List from scratch + addFirst function

---
+++ DSA/src/dataStructures/LinkedListLL.java
@@ -0,0 +1,37 @@
+package dataStructures;
+
+public class LinkedListLL {
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public LinkedListLL() {
+        this.size = 0;
+    }
+
+    public void insertFirst(int val) {
+        Node newNode = new Node(val);
+        newNode.next = head;
+        head = newNode;
+
+        if (tail == null) {
+            tail = head;
+        }
+
+        size++;
+    }
+
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
