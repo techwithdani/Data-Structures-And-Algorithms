c39f322c2d8a6b7d4da384e8035a0802fd74f6db
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-10-03 14:31:56 +0500

    Stacks

--- DSA/src/dataStructures/Stacks.java
+++ DSA/src/dataStructures/Stacks.java
@@ -1,7 +1,21 @@
 package dataStructures;
 
+import java.util.Stack;
+
 public class Stacks {
     public static void main(String[] args) {
-        
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(1);
+        stack.push(4);
+        stack.push(6);
+        stack.push(8);
+        stack.push(10);
+        stack.push(12);
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
     }
 }
