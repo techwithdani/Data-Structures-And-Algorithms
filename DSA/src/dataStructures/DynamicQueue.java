7588df321597a916a512147c14c03bca156cea0e
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-10-07 20:35:35 +0500

    Custom Dynamic Queue

---
+++ DSA/src/dataStructures/DynamicQueue.java
@@ -0,0 +1,27 @@
+package dataStructures;
+
+public class DynamicQueue extends CircularQueue{
+    public DynamicQueue() {
+        super();
+    }
+
+    public DynamicQueue(int size) {
+        super(size);
+    }
+
+    @Override
+    public boolean insert(int item) {
+        if (this.isFull()) {
+            int[] temp = new int[data.length * 2];
+
+            for (int i = 0; i < data.length; i++) {
+                temp[i] = data[(frontPointer + i) % data.length];
+            }
+            frontPointer = 0;
+            endPointer = data.length;
+            data = temp;
+        }
+
+        return super.insert(item);
+    }
+}
