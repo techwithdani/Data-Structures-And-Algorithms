41360c7f369e2a66f9060d8b3dcaa40841a5b8fd
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-10-08 22:02:17 +0500

    Update Main.java

--- DSA/src/Main.java
+++ DSA/src/Main.java
@@ -2,22 +2,6 @@ import dataStructures.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        CircularQueue queue = new CircularQueue(6);
-
-        queue.insert(2);
-        queue.insert(4);
-        queue.insert(6);
-        queue.insert(8);
-        queue.insert(10);
-        queue.insert(12);
-
-        queue.display();
-
-        queue.remove();
-
-        queue.insert(55);
-        System.out.println(queue.peek());
-
-        queue.display();
+        
     }
 }
